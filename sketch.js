4557b4bfc25035b3af4d6c799d78714795b66850
Matt Moore <zmjm1320@gmail.com>
2020-05-11 18:59:35 -0400

    add button to log average frame rate

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 const flock = [];
 const flockCount = 100;
 let frameRate;
+let frameRates = [];
 
 let alignmentSlider, cohesionSlider, separationSlider;
+let frameRateButton;
 
 function setup() {
 	createCanvas(800, 600);
 	alignmentSlider = createSlider(0, 5, 1, .1);
 	cohesionSlider = createSlider(0, 5, 1, .1);
 	separationSlider = createSlider(0, 5, 1, .1);
+
+	frameRateButton = createButton('average frames');
+	frameRateButton.mousePressed(averageFrames);
 
 	for (let i = 0; i < flockCount; i++) {
 		flock.push(new Boid());
@@ -25,6 +30,19 @@ function draw() {
 	}
 	
 	frameRate = getFrameRate();
+	frameRates.push(frameRate);
 	frameRate = floor(frameRate);
 	text(frameRate, 10, 10);
+}
+
+function averageFrames() {
+	let avg = 0;
+
+	frameRates.forEach(element => {
+		avg += element;
+	});
+	avg = avg / frameRates.length;
+	console.log(avg);
+	
+		
 }
